b9d8b5803920f9ab0929f31bdb1de13447b33f2b
AlexSkoett <asha17@student.aau.dk>
2022-04-24 15:58:47 +0200

    Upload changes

--- Miniproj/upload.php
+++ Miniproj/upload.php
@@ -1,16 +1,36 @@
+<!DOCTYPE html>
+
 <html>
-    <body>
+<body>
 
-        <?php
+<?php
 
-        $filename = $_FILES['file']['name'];
+$filename = $_FILES['file']['name'];
 
-        $location = "filestorage/".$filename;
+#$userID = $_GET['id']
+#mkdir("$userID")
+#$location = "$userID/".$filename;
 
-        if( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
-	       echo 'File Upload: Success';}
-        else {
-	       echo 'Error Uploading File';}
-        ?>
-    </body>
+$location = "filestorage/".$filename;
+
+if( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
+	echo 'File Upload: Success';}
+else {
+	echo 'Error Uploading File';}
+	
+$seperateExt = explode('.', $filename);
+$getExt = strtolower(end($seperateExt));
+
+$img = "img/$getExt.png";
+
+if(in_array($getExt, array('doc', 'pdf', 'xls', 'xml', 'zip'))) {
+	echo "<img src='$img'>";}
+else {
+	echo '<img src="img/image.png">';}
+
+#access folder
+?>
+
+
+</body>
 </html>

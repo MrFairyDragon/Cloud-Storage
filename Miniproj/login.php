f13705e60735817f4e7f8f2e394026c50d165abc
Alexskoett <37211748+Alexskoett@users.noreply.github.com>
2022-04-24 12:38:24 +0200

    Added login/register page

---
+++ Miniproj/login.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<form action="uploader.php" method="post">
+			<input type="text" name="username" />
+			<input type="text" name="password"/>
+			<button>login</button>
+		
+</form>
+
+<form action="register.php" method="post">
+			<input type="text" name="uid" />
+			<input type="text" name="pwd"/>
+			<button>register</button>
+
+
+		
+</form>
+
+</body>
+</html>
